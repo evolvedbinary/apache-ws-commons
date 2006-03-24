8d39bfeadd91aa37af32303227d880d5dec67a4a
chinthaka <chinthaka@13f79535-47bb-0310-9956-ffa450edef68>
2006-03-24 04:33:54 +0000

    Moving tags in to correct folders
    git-svn-id: https://svn.apache.org/repos/asf/webservices/commons/tags/policy@388364 13f79535-47bb-0310-9956-ffa450edef68

---
+++ 0_90/src/org/apache/ws/policy/util/StAXPolicyWriter.java
@@ -0,0 +1,267 @@
+/*
+ * Copyright 2001-2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ws.policy.util;
+
+import java.io.OutputStream;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
+import org.apache.ws.policy.AndCompositeAssertion;
+import org.apache.ws.policy.Assertion;
+import org.apache.ws.policy.Policy;
+import org.apache.ws.policy.PolicyConstants;
+import org.apache.ws.policy.PolicyReference;
+import org.apache.ws.policy.PrimitiveAssertion;
+import org.apache.ws.policy.XorCompositeAssertion;
+
+/**
+ * StAXPolicyWriter implements PolicyWriter and provides different methods to
+ * create a Policy object. It uses StAX as its underlying mechanism to create
+ * XML elements.
+ * 
+ * @author Sanka Samaranayake (sanka@apache.org)
+ */
+public class StAXPolicyWriter implements PolicyWriter {
+
+	private int num = 1;
+
+	StAXPolicyWriter() {
+	}
+
+	public void writePolicy(Policy policy, OutputStream output) {
+		XMLStreamWriter writer = null;
+		try {
+			writer = XMLOutputFactory.newInstance().createXMLStreamWriter(
+					output);
+			writePolicy(policy, writer);
+
+			writer.flush();
+
+		} catch (XMLStreamException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+
+	public void writePolicy(Policy policy, XMLStreamWriter writer)
+			throws XMLStreamException {
+		String writerPerfix = writer
+				.getPrefix(PolicyConstants.WS_POLICY_NAMESPACE_URI);
+
+		if (writerPerfix != null) {
+			writer.writeStartElement(PolicyConstants.WS_POLICY_NAMESPACE_URI,
+					PolicyConstants.WS_POLICY);
+
+		} else {
+			writer.writeStartElement(PolicyConstants.WS_POLICY_PREFIX,
+					PolicyConstants.WS_POLICY,
+					PolicyConstants.WS_POLICY_NAMESPACE_URI);
+			writer.writeNamespace(PolicyConstants.WS_POLICY_PREFIX,
+					PolicyConstants.WS_POLICY_NAMESPACE_URI);
+			writer.setPrefix(PolicyConstants.WS_POLICY_PREFIX,
+					PolicyConstants.WS_POLICY_NAMESPACE_URI);
+
+		}
+
+		if (policy.getId() != null) {
+
+			writer.writeNamespace(PolicyConstants.WSU_NAMESPACE_PREFIX,
+					PolicyConstants.WSU_NAMESPACE_URI);
+			writer.setPrefix(PolicyConstants.WSU_NAMESPACE_PREFIX,
+					PolicyConstants.WSU_NAMESPACE_URI);
+
+			writer.writeAttribute("wsu", PolicyConstants.WSU_NAMESPACE_URI,
+					"Id", policy.getId());
+		}
+
+		Iterator iterator = policy.getTerms().iterator();
+		while (iterator.hasNext()) {
+			Assertion term = (Assertion) iterator.next();
+			writeAssertion(term, writer);
+		}
+
+		writer.writeEndElement();
+	}
+
+	private void writeAssertion(Assertion assertion, XMLStreamWriter writer)
+			throws XMLStreamException {
+		if (assertion instanceof PrimitiveAssertion) {
+			writePrimitiveAssertion((PrimitiveAssertion) assertion, writer);
+
+		} else if (assertion instanceof XorCompositeAssertion) {
+			writeXorCompositeAssertion((XorCompositeAssertion) assertion,
+					writer);
+
+		} else if (assertion instanceof PolicyReference) {
+			writePolicyReference((PolicyReference) assertion, writer);
+
+		} else if (assertion instanceof Policy) {
+			writePolicy((Policy) assertion, writer);
+		} else if (assertion instanceof AndCompositeAssertion) {
+			writeAndCompositeAssertion((AndCompositeAssertion) assertion,
+					writer);
+
+		} else {
+			throw new RuntimeException("unknown element type");
+		}
+	}
+
+	private void writeAndCompositeAssertion(AndCompositeAssertion assertion,
+			XMLStreamWriter writer) throws XMLStreamException {
+
+		String writerPrefix = writer
+				.getPrefix(PolicyConstants.WS_POLICY_NAMESPACE_URI);
+
+		if (writerPrefix == null) {
+			writer.writeStartElement(PolicyConstants.WS_POLICY_PREFIX,
+					PolicyConstants.AND_COMPOSITE_ASSERTION,
+					PolicyConstants.WS_POLICY_NAMESPACE_URI);
+			writer.writeNamespace(PolicyConstants.WS_POLICY_PREFIX,
+					PolicyConstants.WS_POLICY_NAMESPACE_URI);
+			writer.setPrefix(PolicyConstants.WS_POLICY_PREFIX,
+					PolicyConstants.WS_POLICY_NAMESPACE_URI);
+
+		} else {
+			writer.writeStartElement(PolicyConstants.WS_POLICY_NAMESPACE_URI,
+					PolicyConstants.AND_COMPOSITE_ASSERTION);
+		}
+
+		List terms = assertion.getTerms();
+		writeTerms(terms, writer);
+
+		writer.writeEndElement();
+	}
+
+	private void writeXorCompositeAssertion(XorCompositeAssertion assertion,
+			XMLStreamWriter writer) throws XMLStreamException {
+		String writerPrefix = writer
+				.getPrefix(PolicyConstants.WS_POLICY_NAMESPACE_URI);
+
+		if (writerPrefix == null) {
+			writer.writeStartElement(PolicyConstants.WS_POLICY_PREFIX,
+					PolicyConstants.XOR_COMPOSITE_ASSERTION,
+					PolicyConstants.WS_POLICY_NAMESPACE_URI);
+			writer.writeNamespace(PolicyConstants.WS_POLICY_PREFIX,
+					PolicyConstants.WS_POLICY_NAMESPACE_URI);
+			writer.setPrefix(PolicyConstants.WS_POLICY_PREFIX,
+					PolicyConstants.WS_POLICY_NAMESPACE_URI);
+
+		} else {
+			writer.writeStartElement(PolicyConstants.WS_POLICY_NAMESPACE_URI,
+					PolicyConstants.XOR_COMPOSITE_ASSERTION);
+		}
+
+		List terms = assertion.getTerms();
+		writeTerms(terms, writer);
+
+		writer.writeEndElement();
+	}
+
+	private void writePrimitiveAssertion(PrimitiveAssertion assertion,
+			XMLStreamWriter writer) throws XMLStreamException {
+		QName qname = assertion.getName();
+
+		String writerPrefix = writer.getPrefix(qname.getNamespaceURI());
+		if (writerPrefix != null) {
+			writer.writeStartElement(qname.getNamespaceURI(), qname
+					.getLocalPart());
+		} else {
+			String prefix = (qname.getPrefix() != null) ? qname.getPrefix()
+					: generateNamespace();
+			writer.writeStartElement(prefix, qname.getLocalPart(), qname
+					.getNamespaceURI());
+			writer.writeNamespace(prefix, qname.getNamespaceURI());
+			writer.setPrefix(prefix, qname.getNamespaceURI());
+
+		}
+
+		Hashtable attributes = assertion.getAttributes();
+		writeAttributes(attributes, writer);
+
+		String text = (String) assertion.getStrValue();
+		if (text != null) {
+			writer.writeCharacters(text);
+		}
+
+		List terms = assertion.getTerms();
+		writeTerms(terms, writer);
+
+		writer.writeEndElement();
+	}
+
+	public void writePolicyReference(PolicyReference assertion,
+			XMLStreamWriter writer) throws XMLStreamException {
+
+		String writerPrefix = writer
+				.getPrefix(PolicyConstants.WS_POLICY_NAMESPACE_URI);
+		if (writerPrefix != null) {
+			writer.writeStartElement(PolicyConstants.WS_POLICY_NAMESPACE_URI,
+					PolicyConstants.WS_POLICY_REFERENCE);
+		} else {
+
+			writer.writeStartElement(PolicyConstants.WS_POLICY_PREFIX,
+					PolicyConstants.WS_POLICY_REFERENCE,
+					PolicyConstants.WS_POLICY_NAMESPACE_URI);
+			writer.writeNamespace(PolicyConstants.WS_POLICY_PREFIX,
+					PolicyConstants.WS_POLICY_NAMESPACE_URI);
+			writer.setPrefix(PolicyConstants.WS_POLICY_PREFIX,
+					PolicyConstants.WS_POLICY_NAMESPACE_URI);
+
+		}
+		writer.writeAttribute("URI", assertion.getPolicyURIString());
+
+		writer.writeEndElement();
+	}
+
+	private void writeTerms(List terms, XMLStreamWriter writer)
+			throws XMLStreamException {
+
+		Iterator iterator = terms.iterator();
+		while (iterator.hasNext()) {
+			Assertion assertion = (Assertion) iterator.next();
+			writeAssertion(assertion, writer);
+		}
+	}
+
+	private void writeAttributes(Hashtable attributes, XMLStreamWriter writer)
+			throws XMLStreamException {
+
+		Iterator iterator = attributes.keySet().iterator();
+		while (iterator.hasNext()) {
+			QName qname = (QName) iterator.next();
+			String value = (String) attributes.get(qname);
+
+			String prefix = qname.getPrefix();
+			if (prefix != null) {
+				writer.writeAttribute(prefix, qname.getNamespaceURI(), qname
+						.getLocalPart(), value);
+			} else {
+				writer.writeAttribute(qname.getNamespaceURI(), qname
+						.getLocalPart(), value);
+			}
+		}
+	}
+
+	private String generateNamespace() {
+		return "ns" + num++;
+	}
+}
---
+++ 1_0/src/org/apache/ws/policy/PolicyConstants.java
@@ -0,0 +1,52 @@
+package org.apache.ws.policy;
+
+/*
+ * Copyright 2001-2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * PolicyConstants interfaces defines some CONST VALUES that are used in the
+ * entier framework.
+ */
+public interface PolicyConstants {
+
+	/** Tag name of AndComposteAssertion */
+	public static final String AND_COMPOSITE_ASSERTION = "All";
+
+	/** Tag name of XorCompositeAssertion */
+	public static final String XOR_COMPOSITE_ASSERTION = "ExactlyOne";
+
+	/** Tag name of Policy */
+	public static final String WS_POLICY = "Policy";
+
+	/** Prefix of WS-Policy namespace */
+	public static final String WS_POLICY_PREFIX = "wsp";
+
+	/** Prefix of WSU namespace */
+	public static final String WSU_NAMESPACE_PREFIX = "wsu";
+
+	/** Tag name of PolicyReference */
+	public static final String WS_POLICY_REFERENCE = "PolicyReference";
+
+	/** Namespace of WS-Policy */
+	public static final String WS_POLICY_NAMESPACE_URI = "http://schemas.xmlsoap.org/ws/2004/09/policy";
+
+	/** Namespace of WSU */
+	public static final String WSU_NAMESPACE_URI = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd";
+
+	/** XML namespace */
+	public static final String XML_NAMESPACE_URI = "http://www.w3.org/XML/1998/namespace";
+
+}
