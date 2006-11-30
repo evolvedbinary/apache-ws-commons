49e5bbd81d6b20bf5d7efe983278547010129964
sanka <sanka@13f79535-47bb-0310-9956-ffa450edef68>
2006-11-30 04:13:40 +0000

    Tagged the branch in preparation for 1.2.1 release.
    git-svn-id: https://svn.apache.org/repos/asf/webservices/commons/tags/axiom@480826 13f79535-47bb-0310-9956-ffa450edef68

---
+++ 1_2_1/modules/axiom-api/src/main/java/org/apache/axiom/om/util/StAXUtils.java
@@ -0,0 +1,217 @@
+package org.apache.axiom.om.util;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
+public class StAXUtils {
+
+    private static interface ObjectCreator {
+		Object newObject();
+	}
+
+	private static class Pool {
+		private final int MAX_POOL_SIZE = 100;
+		private final List objects = new ArrayList();
+		private final ObjectCreator objectCreator;
+		Pool(ObjectCreator[] creators) {
+			ObjectCreator oc = null;
+			for (int i = 0;  i < creators.length;  i++) {
+				try {
+					Object o = creators[i].newObject();
+					oc = creators[i];
+					break;
+				} catch (Throwable t) {
+					// Ignore me
+				}
+			}
+			if (oc == null) {
+				throw new IllegalStateException("No valid ObjectCreator found.");
+			}
+			objectCreator = oc;
+		}
+		synchronized Object getInstance() {
+			final int size = objects.size();
+			if (size > 0) {
+				return objects.remove(size-1);
+			}
+			return objectCreator.newObject();
+		}
+		synchronized void releaseInstance(Object object) {
+			if (objects.size() < MAX_POOL_SIZE) {
+				objects.add(object);
+			}
+		}
+		synchronized void clear() {
+			objects.clear();
+		}
+	}
+
+	private static final Pool xmlInputFactoryPool = new Pool(new ObjectCreator[]{
+	 	new ObjectCreator(){
+			public Object newObject() {
+		        return XMLInputFactory.newInstance("javax.xml.stream.XMLInputFactory", StAXUtils.class.getClassLoader());
+			}
+	 	},
+	 	new ObjectCreator(){
+			public Object newObject() {
+				return XMLInputFactory.newInstance();
+			}
+	 	}
+	});
+
+	private static final Pool xmlOutputFactoryPool = new Pool(new ObjectCreator[]{
+		new ObjectCreator(){
+			public Object newObject() {
+				return XMLOutputFactory.newInstance("javax.xml.stream.XMLOutputFactory", StAXUtils.class.getClassLoader());
+			}
+	 	},
+	 	new ObjectCreator(){
+	 		public Object newObject() {
+	 			return XMLOutputFactory.newInstance();
+			}
+	 	}
+	});
+
+	
+	private static Log log = LogFactory.getLog(StAXUtils.class);
+    private static boolean isDebugEnabled = log.isDebugEnabled();
+	   
+
+	/**
+     * Gets an XMLInputFactory instance from pool.
+     *
+     * @return an XMLInputFactory instance.
+     */
+	public static XMLInputFactory getXMLInputFactory() {
+		return (XMLInputFactory) xmlInputFactoryPool.getInstance();
+    }
+
+    /**
+     * Returns an XMLInputFactory instance for reuse.
+     *
+     * @param factory An XMLInputFactory instance that is available for reuse
+     */
+	public static void releaseXMLInputFactory(XMLInputFactory factory) {
+		xmlInputFactoryPool.releaseInstance(factory);
+    }
+
+    public static XMLStreamReader createXMLStreamReader(InputStream in, String encoding)
+            throws XMLStreamException {
+        XMLInputFactory inputFactory = getXMLInputFactory();
+        try {
+        	XMLStreamReader reader = inputFactory.createXMLStreamReader(in, encoding);
+            if(isDebugEnabled) {
+                log.debug("XMLStreamReader is " + reader.getClass().getName());
+            }
+            return reader;
+        } finally {
+            releaseXMLInputFactory(inputFactory);
+        }
+    }
+
+    public static XMLStreamReader createXMLStreamReader(InputStream in)
+            throws XMLStreamException {
+        XMLInputFactory inputFactory = getXMLInputFactory();
+        try {
+            XMLStreamReader reader = inputFactory.createXMLStreamReader(in);
+            if(isDebugEnabled) {
+                log.debug("XMLStreamReader is " + reader.getClass().getName());
+            }
+            return reader;
+        } finally {
+            releaseXMLInputFactory(inputFactory);
+        }
+    }
+
+    public static XMLStreamReader createXMLStreamReader(Reader in)
+            throws XMLStreamException {
+        XMLInputFactory inputFactory = getXMLInputFactory();
+        try {
+            XMLStreamReader reader = inputFactory.createXMLStreamReader(in);
+            if(isDebugEnabled) {
+                log.debug("XMLStreamReader is " + reader.getClass().getName());
+            }
+            return reader;
+        } finally {
+            releaseXMLInputFactory(inputFactory);
+        }
+    }
+
+    /**
+     * Gets an XMLOutputFactory instance from pool.
+     *
+     * @return an XMLOutputFactory instance.
+     */
+    public static XMLOutputFactory getXMLOutputFactory() {
+    	return (XMLOutputFactory) xmlOutputFactoryPool.getInstance();
+    }
+
+    /**
+     * Returns an XMLOutputFactory instance for reuse.
+     *
+     * @param factory An XMLOutputFactory instance that is available for reuse.
+     */
+    public static void releaseXMLOutputFactory(XMLOutputFactory factory) {
+    	xmlOutputFactoryPool.releaseInstance(factory);
+    }
+
+    public static XMLStreamWriter createXMLStreamWriter(OutputStream out)
+            throws XMLStreamException {
+        XMLOutputFactory outputFactory = getXMLOutputFactory();
+        try {
+            XMLStreamWriter writer = outputFactory.createXMLStreamWriter(out);
+            if(isDebugEnabled) {
+                log.debug("XMLStreamWriter is " + writer.getClass().getName());
+            }
+            return writer;
+        } finally {
+            releaseXMLOutputFactory(outputFactory);
+        }
+    }
+
+    public static XMLStreamWriter createXMLStreamWriter(OutputStream out, String encoding)
+            throws XMLStreamException {
+        XMLOutputFactory outputFactory = getXMLOutputFactory();
+        try {
+            XMLStreamWriter writer = outputFactory.createXMLStreamWriter(out, encoding);
+            if(isDebugEnabled) {
+                log.debug("XMLStreamWriter is " + writer.getClass().getName());
+            }
+            return writer;
+        } finally {
+            releaseXMLOutputFactory(outputFactory);
+        }
+    }
+
+    public static XMLStreamWriter createXMLStreamWriter(Writer out)
+            throws XMLStreamException {
+        XMLOutputFactory outputFactory = getXMLOutputFactory();
+        try {
+            XMLStreamWriter writer = outputFactory.createXMLStreamWriter(out);
+            if(isDebugEnabled) {
+                log.debug("XMLStreamWriter is " + writer.getClass().getName());
+            }
+            return writer;
+        } finally {
+            releaseXMLOutputFactory(outputFactory);
+        }
+    }
+
+    public static void reset() {
+        xmlOutputFactoryPool.clear();
+        xmlInputFactoryPool.clear();
+    }
+}
---
+++ 1_2_1/modules/axiom-tests/src/test/java/org/apache/axiom/om/impl/llom/OMSourcedElementTest.java
@@ -0,0 +1,311 @@
+/*
+ * Copyright 2004,2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.axiom.om.impl.llom;
+
+import org.apache.axiom.om.AbstractTestCase;
+import org.apache.axiom.om.OMDataSource;
+import org.apache.axiom.om.OMDocument;
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMFactory;
+import org.apache.axiom.om.OMNamespace;
+import org.apache.axiom.om.OMNode;
+import org.apache.axiom.om.OMOutputFormat;
+import org.apache.axiom.om.impl.OMNamespaceImpl;
+import org.apache.axiom.om.impl.builder.StAXOMBuilder;
+import org.apache.axiom.om.impl.llom.factory.OMLinkedListImplFactory;
+import org.apache.axiom.om.impl.serialize.StreamingOMSerializer;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Iterator;
+
+/**
+ * Tests the characteristics of OMSourcedElementImpl.
+ */
+public class OMSourcedElementTest extends AbstractTestCase {
+    private static String testDocument =
+        "<library xmlns='http://www.sosnoski.com/uwjws/library' books='1'>" +
+        "<type id='java' category='professional' deductable='true'>"+
+        "<name>Java Reference</name></type><type id='xml' "+
+        "category='professional' deductable='true'><name>XML Reference</name>" +
+        "</type><book isbn='1930110111' type='xml'><title>XSLT Quickly</title>" +
+        "<author>DuCharme, Bob</author><publisher>Manning</publisher>" +
+        "<price>29.95</price></book></library>";
+    
+    private OMSourcedElementImpl element;
+    private OMElement root;
+
+    /**
+     * @param testName
+     */
+    public OMSourcedElementTest(String testName) {
+        super(testName);
+    }
+
+    protected void setUp() throws Exception {
+        OMFactory f = new OMLinkedListImplFactory();
+        OMNamespace ns = new OMNamespaceImpl("http://www.sosnoski.com/uwjws/library", "");
+        OMNamespace rootNS = new OMNamespaceImpl("http://sampleroot", "rootPrefix");
+        element = new OMSourcedElementImpl("library", ns, f, new TestDataSource(testDocument));
+        root = f.createOMElement("root", rootNS);
+        root.addChild(element);
+    }
+    
+    /**
+     * Ensure that each method of OMElementImpl is overridden in OMSourcedElementImpl
+     */
+    public void testMethodOverrides() {
+        Method[] submeths = OMSourcedElementImpl.class.getDeclaredMethods();
+        Method[] supmeths = OMElementImpl.class.getDeclaredMethods();
+        outer: for (int i = 0; i < supmeths.length; i++) {
+            Method supmeth = supmeths[i];
+            Class[] params = supmeth.getParameterTypes();
+            if (!Modifier.isPrivate(supmeth.getModifiers())) {
+                for (int j = 0; j < submeths.length; j++) {
+                    Method submeth = submeths[j];
+                    if (supmeth.getName().equals(submeth.getName())) {
+                        if (Arrays.equals(params, submeth.getParameterTypes())) {
+                            continue outer;
+                        }
+                    }
+                }
+                fail("OMSourcedElementImpl must override method " + supmeth +
+                    "\nSee class JavaDocs for details");
+            }
+        }
+    }
+    
+    private int countItems(Iterator iter) {
+        int count = 0;
+        while (iter.hasNext()) {
+            count++;
+            iter.next();
+        }
+        return count;
+    }
+    
+    /**
+     * Test serialization of OMSourcedElementImpl to a Stream
+     * @throws Exception
+     */
+    public void testSerializeToStream() throws Exception {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        element.serialize(bos);
+        assertEquals("Serialized text error", testDocument,
+            new String(bos.toByteArray()));
+        assertFalse("Element expansion when serializing", element.isExpanded());
+    }
+    
+    /**
+     * Test serialization of OMSourcedElementImpl to a Writer
+     * @throws Exception
+     */
+    public void testSerializeToWriter() throws Exception {
+        StringWriter writer = new StringWriter();
+        element.serialize(writer);
+        String result = writer.toString();
+        assertEquals("Serialized text error", testDocument, result);
+        assertFalse("Element expansion when serializing", element.isExpanded());
+    }
+    
+    /**
+     * Test serialization of OMSourcedElementImpl to an XMLWriter
+     * @throws Exception
+     */
+    public void testSerializeToXMLWriter() throws Exception {
+        StringWriter writer = new StringWriter();
+        XMLStreamWriter xmlwriter = XMLOutputFactory.newInstance().createXMLStreamWriter(writer);
+        element.serialize(writer);
+        xmlwriter.flush();
+        assertEquals("Serialized text error", testDocument, writer.toString());
+        assertFalse("Element expansion when serializing", element.isExpanded());
+    }
+    
+    /**
+     * Tests OMSourcedElement serialization when the root (parent) is serialized.
+     * @throws Exception
+     */
+    public void testSerializeToXMLWriterEmbedded() throws Exception {
+        StringWriter writer = new StringWriter();
+        XMLStreamWriter xmlwriter = XMLOutputFactory.newInstance().createXMLStreamWriter(writer);
+        root.serialize(writer);
+        xmlwriter.flush();
+        String result = writer.toString();
+        // We can't test for equivalence because the underlying OMSourceElement is 
+        // streamed as it is serialized.  So I am testing for an internal value.
+        assertTrue("Serialized text error" + result, result.indexOf("1930110111") > 0);
+        assertFalse("Element expansion when serializing", element.isExpanded());
+    }
+    
+    /**
+     * Tests OMSourcedElement getReader support
+     * @throws Exception
+     */
+    public void testSerializeToXMLWriterFromReader() throws Exception {
+        StringWriter writer = new StringWriter();
+        XMLStreamWriter xmlwriter = XMLOutputFactory.newInstance().createXMLStreamWriter(writer);
+        
+        StAXOMBuilder builder = new StAXOMBuilder(element.getXMLStreamReader());  
+        OMDocument omDocument = builder.getDocument();
+        Iterator it = omDocument.getChildren();
+        while(it.hasNext()) {
+            OMNode omNode = (OMNode) it.next();
+            omNode.serializeAndConsume(xmlwriter);
+        }
+        
+        xmlwriter.flush();
+        String result = writer.toString();
+        // We can't test for equivalence because the underlying OMSourceElement is 
+        // changed as it is serialized.  So I am testing for an internal value.
+        assertTrue("Serialized text error" + result, result.indexOf("1930110111") > 0);
+        assertFalse("Element expansion when serializing", element.isExpanded());
+    }
+    
+    /**
+     * Tests OMSourcedElement processing when the getReader() of the parent is accessed.
+     * @throws Exception
+     */
+    public void testSerializeToXMLWriterFromReaderEmbedded() throws Exception {
+        StringWriter writer = new StringWriter();
+        XMLStreamWriter xmlwriter = XMLOutputFactory.newInstance().createXMLStreamWriter(writer);
+        
+        StAXOMBuilder builder = new StAXOMBuilder(root.getXMLStreamReader());  
+        OMDocument omDocument = builder.getDocument();
+        Iterator it = omDocument.getChildren();
+        while(it.hasNext()) {
+            OMNode omNode = (OMNode) it.next();
+            omNode.serializeAndConsume(xmlwriter);
+        }
+        xmlwriter.flush();
+        String result = writer.toString();
+        // We can't test for equivalence because the underlying OMSourceElement is 
+        // changed as it is serialized.  So I am testing for an internal value.
+        assertTrue("Serialized text error" + result, result.indexOf("1930110111") > 0);
+        // The implementation uses OMNavigator to walk the tree.  Currently OMNavigator must 
+        // expand the OMSourcedElement to correctly walk the elements. (See OMNavigator._getFirstChild)
+        //assertFalse("Element expansion when serializing", element.isExpanded());
+    }
+
+    /**
+     * Make sure the expanded OMSourcedElement behaves like a normal OMElement.
+     * @throws Exception
+     */
+    public void testExpand() throws Exception {
+        element.getAllDeclaredNamespaces();
+        assertEquals("Expanded namespace count error", 1,
+            countItems(element.getAllDeclaredNamespaces()));
+        assertEquals("Expanded attribute count error", 1,
+            countItems(element.getAllAttributes()));
+        assertEquals("Expanded attribute value error", "1",
+            element.getAttributeValue(new QName("books")));
+        OMElement child = element.getFirstElement();
+        assertEquals("Child element name", "type", child.getLocalName());
+        assertEquals("Child element namespace",
+            "http://www.sosnoski.com/uwjws/library", child.getNamespace().getNamespaceURI());
+        OMNode next = child.getNextOMSibling();
+        assertTrue("Expected child element", next instanceof OMElement);
+        next = next.getNextOMSibling();
+        assertTrue("Expected child element", next instanceof OMElement);
+        child = (OMElement)next;
+        assertEquals("Child element name", "book", child.getLocalName());
+        assertEquals("Child element namespace",
+            "http://www.sosnoski.com/uwjws/library", child.getNamespace().getNamespaceURI());
+        assertEquals("Attribute value error", "xml",
+            child.getAttributeValue(new QName("type")));
+    }
+    
+    private static class TestDataSource implements OMDataSource {
+        // The data source is a ByteArrayInputStream so that we can verify that the datasource 
+        // is only accessed once.  Currently there is no way to identify a destructive vs. non-destructive OMDataSource.
+        private final ByteArrayInputStream data;
+        
+        private TestDataSource(String data) {
+            this.data = new ByteArrayInputStream(data.getBytes());
+            this.data.mark(0);
+        }
+
+        /* (non-Javadoc)
+         * @see org.apache.axiom.om.OMDataSource#serialize(java.io.OutputStream, org.apache.axiom.om.OMOutputFormat)
+         */
+        public void serialize(OutputStream output, OMOutputFormat format) throws XMLStreamException {
+            try {
+                output.write(getBytes());
+            } catch (IOException e) {
+                throw new XMLStreamException(e);
+            }
+        }
+
+        /* (non-Javadoc)
+         * @see org.apache.axiom.om.OMDataSource#serialize(java.io.Writer, org.apache.axiom.om.OMOutputFormat)
+         */
+        public void serialize(Writer writer, OMOutputFormat format) throws XMLStreamException {
+            try {
+                writer.write(getString());
+            } catch (IOException e) {
+                throw new XMLStreamException(e);
+            }
+        }
+
+        /* (non-Javadoc)
+         * @see org.apache.axiom.om.OMDataSource#serialize(javax.xml.stream.XMLStreamWriter)
+         */
+        public void serialize(XMLStreamWriter xmlWriter) throws XMLStreamException {
+            StreamingOMSerializer serializer = new StreamingOMSerializer();
+            serializer.serialize(getReader(), xmlWriter);
+        }
+
+        /* (non-Javadoc)
+         * @see org.apache.axiom.om.OMDataSource#getReader()
+         */
+        public XMLStreamReader getReader() throws XMLStreamException {
+            XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+            return inputFactory.createXMLStreamReader(new StringReader(getString()));
+        }
+        
+        private byte[] getBytes() throws XMLStreamException {
+            try {
+                // The data from the data source should only be accessed once
+                //data.reset();
+                byte[] rc = new byte[data.available()];
+                data.read(rc);
+                return rc;
+            } catch (IOException io) {
+                throw new XMLStreamException(io);
+            }
+        }
+        
+        private String getString() throws XMLStreamException {
+            String text = new String(getBytes());
+            return text;
+        }
+    }
+}
