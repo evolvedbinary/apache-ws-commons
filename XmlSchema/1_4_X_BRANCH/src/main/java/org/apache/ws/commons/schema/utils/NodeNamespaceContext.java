5c54df77cc834c35c56ce282eda631b8cdb4b7bc
dkulp <dkulp@13f79535-47bb-0310-9956-ffa450edef68>
2010-09-19 23:21:22 +0000

    Add the reserved prefixs.   Fixes the Woden tests.
    git-svn-id: https://svn.apache.org/repos/asf/webservices/commons/branches/modules@998751 13f79535-47bb-0310-9956-ffa450edef68

--- XmlSchema/1_4_X_BRANCH/src/main/java/org/apache/ws/commons/schema/utils/NodeNamespaceContext.java
+++ XmlSchema/1_4_X_BRANCH/src/main/java/org/apache/ws/commons/schema/utils/NodeNamespaceContext.java
@@ -75,6 +75,11 @@ public class NodeNamespaceContext implements NamespacePrefixList, Serializable {
     
     
     public static String getNamespaceURI(Element el, String pfx) {
+        if ("xml".equals(pfx)) {
+            return "http://www.w3.org/XML/1998/namespace";
+        } else  if ("xmlns".equals(pfx)) {
+            return "http://www.w3.org/2000/xmlns/";
+        }
         if (DOM_LEVEL_3) {
             return getNamespaceURIDomLevel3(el, pfx);
         }
