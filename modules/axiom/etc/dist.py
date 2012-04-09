evolvedbinary/apache-ws-commons

commit 8a161af36cb3c6c65b6b4fb91f8c336adfa85aba

Finalized release notes for 1.2.13 and added an entry to download.html. Also removed the "commons" component from the dist URL for this release.
git-svn-id: https://svn.apache.org/repos/asf/webservices/commons/trunk@1308506 13f79535-47bb-0310-9956-ffa450edef68

modules/axiom/etc/dist.py
@@ -23,8 +23,7 @@ from os.path import *
 from urllib import *
 
 release = argv[1]
-dir = release.replace(".", "_")
-mkdir(dir)
+mkdir(release)
 
 for classifier in [ "bin", "source-release" ]:
 	for suffix in [ "zip", "zip.asc", "zip.md5"]:

modules/axiom/src/site/apt/index.apt
@@ -57,7 +57,7 @@ Key Features
 
 Latest Release
 
-  <<XXX April 2012 - Apache Axiom Version 1.2.13 Released!>>
+  <<2nd April 2012 - Apache Axiom Version 1.2.13 Released!>>
 
   \[{{{http://ws.apache.org/axiom/download.cgi}Download Axiom 1.2.13}}\]
   

modules/axiom/src/site/xdoc/download.xml.vm
@@ -49,23 +49,44 @@
 		
                         <tr class="b">
                             <td align="center" valign="middle">
+                                <a name="1_2_13"></a>
+                                1.2.13
+                            </td>
+                            <td>Source Distribution
+                                <a href="[preferred]/ws/axiom/1.2.13/axiom-1.2.13-source-release.zip">zip</a> |
+                                <a href="http://www.apache.org/dist/ws/axiom/1.2.13/axiom-1.2.13-source-release.zip.md5">MD5</a> |
+                                <a href="http://www.apache.org/dist/ws/axiom/1.2.13/axiom-1.2.13-source-release.zip.asc">PGP</a>
+                                <br/>
+                                Binary Distribution
+                                <a href="[preferred]/ws/axiom/1.2.13/axiom-1.2.13-bin.zip">zip</a> |
+                                <a href="http://www.apache.org/dist/ws/axiom/1.2.13/axiom-1.2.13-bin.zip.md5">MD5</a> |
+                                <a href="http://www.apache.org/dist/ws/axiom/1.2.13/axiom-1.2.13-bin.zip.asc">PGP</a>
+
+                                <br/>
+                            </td>
+                            <td>02 - Apr - 2011</td>
+                            <td>1.2.13 Release (Mirrored)</td>
+                        </tr>
+                        
+                        <tr class="b">
+                            <td align="center" valign="middle">
                                 <a name="1_2_12"></a>
                                 1.2.12
                             </td>
                             <td>Source Distribution
-                                <a href="[preferred]/ws/commons/axiom/1_2_12/axiom-1.2.12-source-release.zip">zip</a> |
-                                <a href="http://www.apache.org/dist/ws/commons/axiom/1_2_12/axiom-1.2.12-source-release.zip.md5">MD5</a> |
-                                <a href="http://www.apache.org/dist/ws/commons/axiom/1_2_12/axiom-1.2.12-source-release.zip.asc">PGP</a>
+                                <a href="http://archive.apache.org/dist/ws/commons/axiom/1_2_12/axiom-1.2.12-source-release.zip">zip</a> |
+                                <a href="http://archive.apache.org/dist/ws/commons/axiom/1_2_12/axiom-1.2.12-source-release.zip.md5">MD5</a> |
+                                <a href="http://archive.apache.org/dist/ws/commons/axiom/1_2_12/axiom-1.2.12-source-release.zip.asc">PGP</a>
                                 <br/>
                                 Binary Distribution
-                                <a href="[preferred]/ws/commons/axiom/1_2_12/axiom-1.2.12-bin.zip">zip</a> |
-                                <a href="http://www.apache.org/dist/ws/commons/axiom/1_2_12/axiom-1.2.12-bin.zip.md5">MD5</a> |
-                                <a href="http://www.apache.org/dist/ws/commons/axiom/1_2_12/axiom-1.2.12-bin.zip.asc">PGP</a>
+                                <a href="http://archive.apache.org/dist/ws/commons/axiom/1_2_12/axiom-1.2.12-bin.zip">zip</a> |
+                                <a href="http://archive.apache.org/dist/ws/commons/axiom/1_2_12/axiom-1.2.12-bin.zip.md5">MD5</a> |
+                                <a href="http://archive.apache.org/dist/ws/commons/axiom/1_2_12/axiom-1.2.12-bin.zip.asc">PGP</a>
 
                                 <br/>
                             </td>
                             <td>02 - Jul - 2011</td>
-                            <td>1.2.12 Release (Mirrored)</td>
+                            <td>1.2.12 Release (Archived)</td>
                         </tr>
                         
                         <tr class="b">

commit 8f759bb5bc90181f0309fd5c904452ecede5a999

Fixed a bug in the dist.py script introduced in r1308506.
git-svn-id: https://svn.apache.org/repos/asf/webservices/commons/trunk@1311182 13f79535-47bb-0310-9956-ffa450edef68

modules/axiom/etc/dist.py
@@ -28,5 +28,5 @@ mkdir(release)
 for classifier in [ "bin", "source-release" ]:
 	for suffix in [ "zip", "zip.asc", "zip.md5"]:
 		file = "axiom-" + release + "-" + classifier + "." + suffix
-		urlretrieve("http://repository.apache.org/content/repositories/releases/org/apache/ws/commons/axiom/axiom/" + release + "/" + file, join(dir, file))
+		urlretrieve("http://repository.apache.org/content/repositories/releases/org/apache/ws/commons/axiom/axiom/" + release + "/" + file, join(release, file))
 