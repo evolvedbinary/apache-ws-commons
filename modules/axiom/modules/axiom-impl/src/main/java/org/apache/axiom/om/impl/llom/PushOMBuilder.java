evolvedbinary/apache-ws-commons

commit 3869598060f9735e4bd67878cf9b39af1e135036

AXIOM-420: Fixed an issue that occurs with elements using a default namespace declared on an ancestor.
git-svn-id: https://svn.apache.org/repos/asf/webservices/commons/trunk@1340671 13f79535-47bb-0310-9956-ffa450edef68

modules/axiom/modules/axiom-impl/src/main/java/org/apache/axiom/om/impl/llom/PushOMBuilder.java
@@ -109,8 +109,9 @@ public class PushOMBuilder extends AbstractXMLStreamWriter implements DataHandle
             root.validateName(prefix, localName, namespaceURI);
             parent = root;
         } else {
-            // We don't pass the namespace here in order to avoid creation of a namespace declaration
-            parent = factory.createOMElement(localName, null, parent);
+            // We use the createOMElement variant that takes a OMXMLParserWrapper parameter and
+            // don't pass the namespace. This avoids creation of a namespace declaration.
+            parent = factory.createOMElement(localName, null, parent, null);
         }
         if (ns != null) {
             parent.setNamespaceWithNoFindInCurrentScope(ns);

modules/axiom/modules/axiom-testsuite/src/main/java/org/apache/axiom/ts/om/OMTestSuiteBuilder.java
@@ -399,6 +399,7 @@ public class OMTestSuiteBuilder extends TestSuiteBuilder {
             addTest(new org.apache.axiom.ts.om.sourcedelement.push.TestWriteNamespace(metaFactory, "", "urn:test"));
             addTest(new org.apache.axiom.ts.om.sourcedelement.push.TestWriteNamespace(metaFactory, "p", "urn:test"));
             addTest(new org.apache.axiom.ts.om.sourcedelement.push.TestWriteStartElementAutoPrefix(metaFactory));
+            addTest(new org.apache.axiom.ts.om.sourcedelement.push.TestWriteStartElementWithDefaultNamespaceDeclaredOnParent(metaFactory));
         }
         addTest(new org.apache.axiom.ts.om.pi.TestDigest(metaFactory));
         addTest(new org.apache.axiom.ts.om.text.TestBase64Streaming(metaFactory));

modules/axiom/modules/axiom-testsuite/src/main/java/org/apache/axiom/ts/om/sourcedelement/push/TestWriteStartElementWithDefaultNamespaceDeclaredOnParent.java
@@ -1,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.axiom.ts.om.sourcedelement.push;
+
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMFactory;
+import org.apache.axiom.om.OMMetaFactory;
+import org.apache.axiom.om.ds.AbstractPushOMDataSource;
+import org.apache.axiom.ts.AxiomTestCase;
+
+/**
+ * Tests that {@link XMLStreamWriter#writeStartElement(String, String, String)} creates the correct
+ * {@link OMElement} if the element uses a default namespace declared on the parent.
+ */
+public class TestWriteStartElementWithDefaultNamespaceDeclaredOnParent extends AxiomTestCase {
+    public TestWriteStartElementWithDefaultNamespaceDeclaredOnParent(OMMetaFactory metaFactory) {
+        super(metaFactory);
+    }
+
+    protected void runTest() throws Throwable {
+        OMFactory factory = metaFactory.getOMFactory();
+        OMElement element = factory.createOMElement(new AbstractPushOMDataSource() {
+            public void serialize(XMLStreamWriter writer) throws XMLStreamException {
+                writer.writeStartElement("", "root", "urn:test");
+                writer.writeDefaultNamespace("urn:test");
+                writer.setDefaultNamespace("urn:test");
+                writer.writeStartElement("", "child", "urn:test");
+                writer.writeCharacters("test");
+                writer.writeEndElement();
+                writer.writeEndElement();
+            }
+            
+            public boolean isDestructiveWrite() {
+                return false;
+            }
+        });
+        OMElement child = element.getFirstElement();
+        assertNull(child.getPrefix());
+        assertEquals("urn:test", child.getNamespaceURI());
+        assertEquals("child", child.getLocalName());
+        assertFalse(child.getAllDeclaredNamespaces().hasNext());
+    }
+}

commit 3c8ba4641970d10ae77cacd57fea0d6772f57131

Fixed a regression introduced by r1340671.
git-svn-id: https://svn.apache.org/repos/asf/webservices/commons/trunk@1340940 13f79535-47bb-0310-9956-ffa450edef68

modules/axiom/modules/axiom-impl/src/main/java/org/apache/axiom/om/impl/llom/PushOMBuilder.java
@@ -33,6 +33,7 @@ import org.apache.axiom.om.OMFactory;
 import org.apache.axiom.om.OMNamespace;
 import org.apache.axiom.om.OMNode;
 import org.apache.axiom.om.OMText;
+import org.apache.axiom.om.impl.OMContainerEx;
 import org.apache.axiom.util.stax.AbstractXMLStreamWriter;
 
 public class PushOMBuilder extends AbstractXMLStreamWriter implements DataHandlerWriter {
@@ -126,6 +127,9 @@ public class PushOMBuilder extends AbstractXMLStreamWriter implements DataHandle
         if (parent == root) {
             parent = null;
         } else {
+            // Since we use the createOMElement variant that takes a OMXMLParserWrapper parameter,
+            // we need to update the completion status.
+            ((OMContainerEx)parent).setComplete(true);
             parent = (OMElement)parent.getParent();
         }
     }

modules/axiom/modules/axiom-testsuite/src/main/java/org/apache/axiom/ts/om/sourcedelement/PushOMDataSource.java
@@ -27,6 +27,7 @@ import org.apache.axiom.om.ds.AbstractPushOMDataSource;
 public class PushOMDataSource extends AbstractPushOMDataSource implements QNameAwareOMDataSource {
     public void serialize(XMLStreamWriter writer) throws XMLStreamException {
         writer.writeStartElement("p", "root", "urn:test");
+        writer.writeEmptyElement("p", "child", "urn:test");
         writer.writeEndElement();
     }
 

modules/axiom/modules/axiom-testsuite/src/main/java/org/apache/axiom/ts/om/sourcedelement/TestGetXMLStreamReaderWithPushOMDataSource.java
@@ -46,5 +46,12 @@ public class TestGetXMLStreamReaderWithPushOMDataSource extends AxiomTestCase {
         assertTrue(element.isExpanded());
         // In addition, if the element is expanded, it will always be complete
         assertTrue(element.isComplete());
+        
+        // Verify remaining events
+        assertEquals(XMLStreamReader.START_ELEMENT, reader.next());
+        assertEquals("child", reader.getLocalName());
+        assertEquals(XMLStreamReader.END_ELEMENT, reader.next());
+        assertEquals(XMLStreamReader.END_ELEMENT, reader.next());
+        assertEquals(XMLStreamReader.END_DOCUMENT, reader.next());
     }
 }